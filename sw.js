782f17fcb38911f85c91081a2f8fdd0f4d8a0f68
Guticode21 <alejandrogutierrez053@gmail.com>
2026-08-17 11:58:04 -0400

    fix: actualizar llaves de firebase al proyecto correcto mundo-romix

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Maneja caché offline y notificaciones diarias
    ============================================= */
 
-const CACHE_NAME = 'mundo-romix-v1.4';
+const CACHE_NAME = 'mundo-romix-v1.7';
 const ARCHIVOS_CACHE = [
   './',
   './index.html',
